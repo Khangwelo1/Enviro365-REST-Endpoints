92b8d8baacd53f76dfc778322e1d5b97538232e1
Khangwelo1 <khangwelokevinm@gmail.com>
2024-06-28 13:09:18 +0200

    The repo of my project

---
+++ src/main/java/com/enviro/assessment/grad001/KhangweloKevinMamatho/Repo/DisposalGuidelineRepo.java
@@ -0,0 +1,9 @@
+package com.enviro.assessment.grad001.KhangweloKevinMamatho.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enviro.assessment.grad001.KhangweloKevinMamatho.Entities.DisposalGuideline;
+
+public interface DisposalGuidelineRepo extends JpaRepository<DisposalGuideline, Long> {
+    
+}
---
+++ src/main/java/com/enviro/assessment/grad001/KhangweloKevinMamatho/Repo/RecyclingTipsRepo.java
@@ -0,0 +1,9 @@
+package com.enviro.assessment.grad001.KhangweloKevinMamatho.Repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.enviro.assessment.grad001.KhangweloKevinMamatho.Entities.RecyclingTips;
+
+public interface RecyclingTipsRepo extends JpaRepository<RecyclingTips, Long> {
+    
+}
