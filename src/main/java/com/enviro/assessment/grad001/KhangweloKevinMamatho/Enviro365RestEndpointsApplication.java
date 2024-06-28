487b434bd6edb717f1edca34b7a0bf5ecb9ee9e3
Khangwelo1 <khangwelokevinm@gmail.com>
2024-06-28 13:16:13 +0200

    Main Class of the Application(MainStarter)

--- src/main/java/com/enviro/assessment/grad001/KhangweloKevinMamatho/Enviro365RestEndpointsApplication.java
+++ src/main/java/com/enviro/assessment/grad001/KhangweloKevinMamatho/Enviro365RestEndpointsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Enviro365RestEndpointsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Enviro365RestEndpointsApplication.class, args);
 	}
-
 }
