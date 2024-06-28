bb913cdf9d3f9b7743895a1c43283e94a59f0f0a
Khangwelo1 <khangwelokevinm@gmail.com>
2024-06-28 13:06:00 +0200

    Main Class of the Application(MainStarter)

---
+++ src/main/java/com/enviro/assessment/grad001/KhangweloKevinMamatho/Enviro365RestEndpointsApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.KhangweloKevinMamatho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365RestEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365RestEndpointsApplication.class, args);
+	}
+
+}
